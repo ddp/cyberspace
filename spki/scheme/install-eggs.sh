b78f37aacb560303bb80a767c39ccd39aefc21f1
Derrell Piper <ddp@electric-loft.org>
2026-01-09 19:54:53 -0800

    Add eggs dependency list and install script

---
+++ spki/scheme/install-eggs.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Install required eggs for Cyberspace Scheme
+# Run: ./install-eggs.sh
+
+set -e
+
+EGGS="srfi-1 srfi-4 srfi-13 srfi-14 srfi-18 srfi-69"
+
+echo "Installing Cyberspace Scheme dependencies..."
+for egg in $EGGS; do
+    echo "  $egg"
+    chicken-install "$egg"
+done
+
+echo "Done. Run: cyberspace"
